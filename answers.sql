5a188611b3c0458a7537ef87620055d16fddea7b
Dearbeey <oliveromondy@gmail.com>
2025-05-03 19:19:42 +0300

    Week 7 Database assignment Submission

---
+++ answers.sql
@@ -0,0 +1,46 @@
+-- Question one 
+-- 1NF
+SELECT 101 AS OrderID, 'John Doe' AS CustomerName, 'Laptop' AS Product
+UNION ALL
+SELECT 101, 'John Doe', 'Mouse'
+UNION ALL
+SELECT 102, 'Jane Smith', 'Tablet'
+UNION ALL
+SELECT 102, 'Jane Smith', 'Keyboard'
+UNION ALL
+SELECT 102, 'Jane Smith', 'Mouse'
+UNION ALL
+SELECT 103, 'Emily Clark', 'Phone';
+
+-- Question Two
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(100)
+);
+
+-- Insert unique orders
+INSERT INTO Orders (OrderID, CustomerName) VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
+
+
+-- composite key = (OrderID, Product)
+CREATE TABLE OrderItems (
+    OrderID INT,
+    Product VARCHAR(50),
+    Quantity INT,
+    PRIMARY KEY (OrderID, Product),
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+
+-- Insert item
+INSERT INTO OrderItems (OrderID, Product, Quantity) VALUES
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
+
+
